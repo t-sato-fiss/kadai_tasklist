9f586f9e409a1c375ccbf8e95375076d39d50ac6
t.sato <t-sato@fissc.co.jp>
2025-05-28 14:25:37 +0900

    Task_CreateServlet.javaを作成

---
+++ src/main/java/controllers/Task_CreateServlet.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import jakarta.persistence.EntityManager;
+import models.Task;
+import utils.Task_DBUtil;
+import java.sql.Timestamp;
+/**
+ * Servlet implementation class Task_CreateServlet
+ */
+@WebServlet("/task_create")
+public class Task_CreateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Task_CreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = Task_DBUtil.createEntityManager();
+            em.getTransaction().begin();
+
+            Task t = new Task();
+
+            String content = request.getParameter("content");
+            t.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            t.setCreated_at(currentTime);
+            t.setUpdated_at(currentTime);
+
+            em.persist(t);
+            em.getTransaction().commit();
+            em.close();
+
+            response.sendRedirect(request.getContextPath() + "/task_index");
+        }
+    }
+
+}
