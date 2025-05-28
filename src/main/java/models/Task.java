2c14ef5e8a914eac8f5ba3f360feea2c5b28e2ea
t.sato <t-sato@fissc.co.jp>
2025-05-28 16:49:15 +0900

    ページネーションを追加

--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -15,7 +15,11 @@ import jakarta.persistence.NamedQuery;
         @NamedQuery(
                 name = "getAllTasks",
                 query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
-        )
+        ),
+        @NamedQuery(
+                name = "getTasksCount",
+                query = "SELECT COUNT(t) FROM Task AS t"
+                )
 })
 @Table(name = "tasks")
 
