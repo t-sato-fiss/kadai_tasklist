4f28ff7f09b3e6f6fc47f5b7c585c6ac9ba53570
t.sato <t-sato@fissc.co.jp>
2025-05-28 14:41:40 +0900

    Task_UpdateServlet.javaを作成

---
+++ src/main/java/controllers/Task_UpdateServlet.java
@@ -0,0 +1,60 @@
+package controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import jakarta.persistence.EntityManager;
+import models.Task;
+import utils.Task_DBUtil;
+import java.sql.Timestamp;
+/**
+ * Servlet implementation class Task_UpdateServlet
+ */
+@WebServlet("/task_update")
+public class Task_UpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Task_UpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = Task_DBUtil.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+            // フォームの内容を各フィールドに上書き
+            String content = request.getParameter("content");
+            t.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            t.setUpdated_at(currentTime);       // 更新日時のみ上書き
+
+            // データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("task_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/task_index");
+        }
+    }
+
+}
